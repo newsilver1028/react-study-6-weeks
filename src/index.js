3b5c985c2b80f90fb7030a76f48638ad0146dd93
GAEUN SHIN <newsilver1028@naver.com>
2022-02-26 14:14:58 +0900

    Change createStore to configureStore

--- src/index.js
+++ src/index.js
@@ -10,6 +10,8 @@ import { composeWithDevTools } from "redux-devtools-extension";
 import rootReducer from './reducers';
 import ReduxThunk from 'redux-thunk';
 
+import { configureStore } from '@reduxjs/toolkit';
+
 // 배포 레벨에서는 리덕스 발동시 찍히는 logger를 사용하지 않습니다.
 const enhancer =
   process.env.NODE_ENV === "production"
@@ -17,7 +19,8 @@ const enhancer =
     : composeWithDevTools(applyMiddleware(logger,ReduxThunk));
 
 // 위에서 만든 reducer를 스토어 만들때 넣어줍니다
-const store = createStore(rootReducer, enhancer);
+// const store = createStore(rootReducer, enhancer);
+const store = configureStore({ reducer: rootReducer })
 
 ReactDOM.render(
   <Provider store={store}>
